93c13130d1ef9f70b10d1de28caec669623b3eee
Layanne Cantanhede <layrodrgs@gmail.com>
2020-08-05 15:57:41 -0300

    TESTE

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Routes from './routes';
+import Globalstyle from './styles/global';
+
+function App() {
+  return(
+      <>
+      <Routes />
+      <Globalstyle/>
+      </>
+  );
+
+}
+
+export default App;
